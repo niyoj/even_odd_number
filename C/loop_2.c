bcfdb926805837ea285e794ab0be0b29ac9aa734
NiyojOli <oliniyoj@gmail.com>
2021-07-19 08:15:50 +0545

    Fixes some minor mistakes in README.md and loop_2.c

--- C/loop_2.c
+++ C/loop_2.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-int main()
-{
+
+int main(void) {
     int num, i, temp;
-    printf("Enter any integer:- ");
+    
+    printf("Enter any integer: ");
     scanf("%d", &num);
+    
     temp = num;
-    for (i=1; i<=(temp/2); i++)
-    {
+    
+    for (i=1; i<=(temp/2); i++) 
         num = num-2;
-    }
+    
     if (num==0)
-    {
         printf("%d is even number. \n", temp);
-    }
     else
-    {
         printf("%d is odd number. \n", temp);
-    }
+
     return 0;
 }
