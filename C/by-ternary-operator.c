e85fb85080f6bd9b8302484243e3f995c8a0b1b7
Khemraj-bit <86350457+Khemraj-bit@users.noreply.github.com>
2021-07-19 07:44:43 +0545

    by-ternary-operator

--- C/by-ternary-operator.c
+++ C/by-ternary-operator.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-void oddeven (int);
 int main()
 {
     int num;
     printf("Enter number to check odd or even:- ");
     scanf("%d", &num);
-    oddeven (num);
-    return 0;
-}
-void oddeven (int num)
-{
     (num%2!=0) ? printf("%d is odd number. \n", num): printf("%d is even number. \n", num);
+    return 0;
 }
